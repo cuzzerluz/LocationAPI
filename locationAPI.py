fcf8864836b4b432d82b88265ebec1ce7a0db952
cuzzerluz <37315404+cuzzerluz@users.noreply.github.com>
2018-10-01 13:56:25 +0100

    Changes
    Changes

---
+++ locationAPI.py
@@ -0,0 +1,76 @@
+import pymysql
+
+from flask import (
+    Flask,
+    render_template
+)
+
+# Create the application instance
+app = Flask(__name__, template_folder="templates")
+
+def dbConnecttest(type):
+    result = type
+    return (result)
+
+def dbConnect(type):
+    connection = pymysql.connect(host='localhost',
+                                 user='dave',
+                                 password='zzz',
+                                 db='db',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    if type == "add": #Add a new record into the db
+        try:
+            with connection.cursor() as cursor:
+                #Create
+                sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+                cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+            connection.commit()
+        finally:
+            connection.close()
+    elif type == "read": #Read a record from the db
+        try:
+            with connection.cursor() as cursor:
+                # Read a single record
+                sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
+                cursor.execute(sql, ('webmaster@python.org',))
+                result = cursor.fetchone()
+                print(result) 
+                return (result)
+        finally:
+            connection.close()        
+    elif type == "delete": #Delete the records from the DB 
+        try:
+            with connection.cursor() as cursor:
+                # Read a single record
+                sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
+                cursor.execute(sql, ('webmaster@python.org',))
+                result = cursor.fetchone()
+                print(result)    
+        finally:
+            connection.close()
+    else:
+        connection.close()        
+                
+                    
+    
+
+# Create a URL route in our application for "/"
+@app.route('/')
+def home():
+    """
+    This function just responds to the browser ULR
+    localhost:5000/
+
+    :return:        the rendered template 'home.html'
+    """
+    return render_template('home.html')
+
+@app.route('/current')
+def current():
+    currentrec = dbConnecttest('read')   
+    return currentrec
+
+# If we're running in stand alone mode, run the application
+if __name__ == '__main__':
+    app.run(debug=True)
